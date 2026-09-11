7c2a752e0a336ce34cc12b6fc1fc971e599b1331
Julia Aquilino Pretell <juliaaquilinoo@gmail.com>
2026-09-11 11:42:24 -0400

    added to test file. testing capitalization, correct and incorrect answers

--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest {
+    @Test
+    void correctAnswerReturnsTrue() {
+
+        Game game = new Game();
+        boolean result = game.checkAnswer("lovely", "lovely");
+        assertTrue(result);
+    }
+
+    @Test
+    void incorrectAnswerReturnsFalse() {
+        Game game = new Game();
+        boolean result = game.checkAnswer("summer", "lovely");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void capitalizationDoesNotMatter() {
+        Game game = new Game();
+        boolean result = game.checkAnswer("LOVELY", "lovely");
+
+        assertTrue(result);
+    }
 }
