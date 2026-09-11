1ba851bb0f0f7a350d8ba006fec96a129b6d15d4
Julia Aquilino Pretell <juliaaquilinoo@gmail.com>
2026-09-11 10:43:33 -0400

    initializing game variables

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
 
     private Sonnet sonnet;
+    private Scanner scanner;
+    private Random random;
+
+    private int correct;
+    private int incorrect;
 
     public Game() {
+
         sonnet = new Sonnet();
+        scanner = new Scanner(System.in);
+        random = new Random();
+
+        correct = 0;
+        incorrect = 0;
     }
 
     public void start() {
