645da69dcf21956b04923b27080225167b900d70
Julia Aquilino Pretell <juliaaquilinoo@gmail.com>
2026-09-11 11:51:06 -0400

    sonnet test

--- src/test/java/org/example/SonnetTest.java
+++ src/test/java/org/example/SonnetTest.java
@@ -1,4 +1,28 @@
 package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SonnetTest {
+
+    @Test
+    void sonnetHasFourteenLines() {
+        Sonnet sonnet = new Sonnet();
+        String[] lines = sonnet.getLines();
+        assertEquals(14, lines.length);
+    }
+
+    @Test
+    void sonnetStartWithCorrectLine() {
+        Sonnet sonnet = new Sonnet();
+        String[] lines = sonnet.getLines();
+        assertEquals("Shall I compare thee to a summer's day?", lines[0]);
+    }
+
+    @Test
+    void sonnetEndsWithCorrectLine() {
+        Sonnet sonnet = new Sonnet();
+        String[] lines = sonnet.getLines();
+
+        assertEquals("So long lives this, and this gives life to thee.", lines[13]);
+    }
 }
