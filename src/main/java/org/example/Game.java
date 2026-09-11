b48eb03b1e8b77c02a6b0b46107430c7d4dc5ebd
Julia Aquilino Pretell <juliaaquilinoo@gmail.com>
2026-09-11 11:22:10 -0400

    testing game in main

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -32,7 +32,7 @@ public class Game {
             int randomWord = random.nextInt(words.length);
 
             for (int i = 0; i < randomLine; i++) {
-                System.out.print(words[i] + " ");
+                System.out.println(lines[i]);
             }
             for (int i = 0; i < randomWord; i++) {
                 System.out.print(words[i] + " ");
@@ -43,7 +43,7 @@ public class Game {
             System.out.print("What is the missing word?");
             String answer = scanner.nextLine();
 
-            String correctWord = words[randomWord].replaceAll("[^a-zA-A]", "");
+            String correctWord = words[randomWord].replaceAll("[^a-zA-Z]", "");
 
             if (answer.equalsIgnoreCase(correctWord)) {
                 System.out.println("Correct!");
@@ -55,8 +55,11 @@ public class Game {
             System.out.println();
         }
 
-        for (String line : lines) {
-            System.out.println(line);
+        if (correct == 3) {
+            System.out.println("You got three words correct!");
+        } else {
+            System.out.println("You got three words incorrect.");
         }
+
     }
 }
