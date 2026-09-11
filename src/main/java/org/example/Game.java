21f3130233daa2a985b1fbf1a664c3b61361e6a1
Julia Aquilino Pretell <juliaaquilinoo@gmail.com>
2026-09-11 10:56:07 -0400

    initialized correct and incorrect answers, imported random for word replacement and message for correct and incorrect answers

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -23,7 +23,37 @@ public class Game {
     }
 
     public void start() {
-        String[] lines = sonnet.getLines();
+        while (correct < 3 && incorrect < 3) {
+
+            String[] lines = sonnet.getLines();
+            int randomLine = random.nextInt(lines.length);
+
+            String[] words = lines[randomLine].split(" ");
+            int randomWord = random.nextInt(words.length);
+
+            for (int i = 0; i < randomLine; i++) {
+                System.out.print(words[i] + " ");
+            }
+            for (int i = 0; i < randomWord; i++) {
+                System.out.print(words[i] + " ");
+            }
+
+            System.out.println("____");
+
+            System.out.print("What is the missing word?");
+            String answer = scanner.nextLine();
+
+            String correctWord = words[randomWord].replaceAll("[^a-zA-A]", "");
+
+            if (answer.equalsIgnoreCase(correctWord)) {
+                System.out.println("Correct!");
+                correct++;
+            } else {
+                System.out.println("Incorrect. The word was:" + correctWord);
+                incorrect++;
+            }
+            System.out.println();
+        }
 
         for (String line : lines) {
             System.out.println(line);
